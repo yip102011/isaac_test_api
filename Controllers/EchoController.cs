32c0be3c814a0dbd82aae3c95866e0f5c718d419
Isaac Yip <yip102011@gmail.com>
2022-04-19 03:12:27 +0800

    Add project files.

---
+++ Controllers/EchoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace isaac_test_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        // echo function 
+
+
+
+
+    }
+}
